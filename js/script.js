88b7fb01600d69420385c8c6baa154dd2a6b2c22
Mattia Bocchino <pmattiab@live.it>
2021-04-22 12:41:45 +0200

    struttura

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// var per vue js
+var app = new Vue(
+    {
+        // elemento con id root
+        el: "#root",
+
+        // data
+        data: {},
+
+        // methods (funzioni)
+        methods: {},
+
+        // funzioni da far partire al caricamento della pagina
+        created() {}
+    }
+);
